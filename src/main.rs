61f2b5d0cfe6deda11305b3c7a4ea891c68532dd
Perry Hertler <perry@hertler.org>
2024-04-02 19:44:58 -0500

    pong

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+use anyhow::Context;
+
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").context("unable to bind tcp listener")?;
     
     for stream in listener.incoming() {
          match stream {
-             Ok(_stream) => {
+             Ok(mut stream) => {
                  println!("accepted new connection");
+                 let pong = b"+PONG\r\n";
+                 stream.write_all(pong).context("unable to write to stream")?;
              }
              Err(e) => {
                  println!("error: {}", e);
              }
          }
      }
+     Ok(())
 }
