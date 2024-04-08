5e1b238b990b5595793b2212b20c4f5dde9230f5
Perry Hertler <perry@hertler.org>
2024-04-08 07:48:02 -0500

    listening port and capability

--- src/replica_slave.rs
+++ src/replica_slave.rs
@@ -1,9 +1,10 @@
-use anyhow::ensure;
 use bytes::Bytes;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 
 use crate::{
-    info::Info, resp_lexer::{RESPArray, RESPBulkString, RESPValue, Serialize}, store::Store
+    info::Info,
+    resp_lexer::{RESPArray, RESPBulkString, RESPValue, Serialize},
+    store::Store,
 };
 
 pub async fn slave_hand_shake(store: &Store) -> anyhow::Result<()> {
@@ -11,29 +12,19 @@ pub async fn slave_hand_shake(store: &Store) -> anyhow::Result<()> {
     let master_address = info.replication.master_address()?;
     let mut stream = tokio::net::TcpStream::connect(master_address).await?;
 
-    let (reader, writer) = stream.split();
-    ping(reader, writer).await
-}
+    let (_reader, mut writer) = stream.split();
 
-async fn ping<Reader, Writer>(mut reader: Reader, mut writer: Writer) -> anyhow::Result<()>
-where
-    Reader: AsyncReadExt + Unpin,
-    Writer: AsyncWriteExt + Unpin,
-{
     let ping_bytes = ping_bytes().await?;
     writer.write_all(&ping_bytes).await?;
 
-    let mut buf = [0; 512];
-    let byte_count = reader.read(&mut buf).await?;
-    ensure!(byte_count > 0, "No data received from master");
-    let bytes = &buf[..byte_count];
-    let mut lexer = crate::resp_lexer::Lexer::new(bytes.into());
-    let pong = lexer.lex()?;
-    println!("Received value: {:?}", &pong);
+    let listening_port_bytes = listening_port_bytes(&info).await?;
+    writer.write_all(&listening_port_bytes).await?;
+
+    let cap_bytes = capability_bytes().await?;
+    writer.write_all(&cap_bytes).await?;
 
     Ok(())
 }
-
 async fn ping_bytes() -> anyhow::Result<Bytes> {
     let bytes = RESPArray {
         data: vec![RESPValue::BulkString(RESPBulkString::new(Bytes::from(
@@ -86,7 +77,8 @@ mod tests {
             self_port: 6380,
             ..Default::default()
         };
-        let expected_bytes = Bytes::from("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n");    
+        let expected_bytes =
+            Bytes::from("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n");
         let bytes = listening_port_bytes(&info).await?;
         assert_eq!(bytes, expected_bytes);
         Ok(())
@@ -94,18 +86,18 @@ mod tests {
 
     #[tokio::test]
     async fn test_capability_bytes() -> anyhow::Result<()> {
-        let expected_bytes = Bytes::from("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");    
+        let expected_bytes = Bytes::from("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
         let bytes = capability_bytes().await?;
         assert_eq!(bytes, expected_bytes);
         Ok(())
     }
 
-    #[tokio::test]
-    async fn test_ping() {
-        let reader = tokio_test::io::Builder::new().read(b"+PONG\r\n").build();
-        let writer = tokio_test::io::Builder::new()
-            .write(b"*1\r\n$4\r\nPING\r\n")
-            .build();
-        let _ = ping(reader, writer).await;
-    }
+    // #[tokio::test]
+    // async fn test_ping() {
+    //     let reader = tokio_test::io::Builder::new().read(b"+PONG\r\n").build();
+    //     let writer = tokio_test::io::Builder::new()
+    //         .write(b"*1\r\n$4\r\nPING\r\n")
+    //         .build();
+    //     let _ = ping(reader, writer).await;
+    // }
 }
