04da24223cf9f1ed321a56f9e0548180f09fd240
Perry Hertler <perry@hertler.org>
2024-04-14 10:01:09 -0500

    starting over

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-use redis_starter_rust::{
-    redis_args::RedisArgs, replica_slave::slave_hand_shake, server::run, store::Store,
-};
 use std::env;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = RedisArgs::parse(env::args().collect::<Vec<String>>())?;
-    let store = Store::new();
-    let info = args.to_info();
-    info.write(&store)?;
-
-    if info.is_slave() {
-        slave_hand_shake(&store).await?;
-    }
-
-    run(&store).await
+    Ok(())
 }
